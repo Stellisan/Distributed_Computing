76f290861c1cbad1d40ac29d9156cfd1a6d3ab63
Sethu Manickam <sethu.sxcs@gmail.com>
2020-10-07 18:51:18 -0700

    Update q.h
    Added docs for functions

--- Project_2/q.h
+++ Project_2/q.h
@@ -3,6 +3,13 @@
 #include<errno.h>
 #include "TCB.h"
 
+/*
+* Group members: 
+* 1. Sanjay Arivazhagan; ASURITE ID: 1217643921; sarivazh@asu.edu 
+* 2. Sethu Manickam; ASURITE ID: 1218452066; smanick4@asu.edu
+*/
+
+
 struct q_element{
 	struct q_element *pPrev, *pNext;
 	int iValue;
@@ -11,12 +18,24 @@ struct q_element{
 
 typedef struct q_element q_element;
 
+/*
+* Function Name: newQueue
+* Parameters: None
+* Return type: TCB_t*
+* Description: Function to return a new queue of TCB_t's
+*/
 struct TCB_t* newQueue()
 {
 	struct TCB_t *queue = NULL;
 	return queue;
 }
 
+/*
+* Function Name: newItem
+* Parameters: None
+* Return type: TCB_t*
+* Description: Function to return a pointer to a new element, uses memory allocation
+*/
 struct TCB_t* newItem()
 {
 	struct TCB_t *new_item = (struct TCB_t*)malloc(sizeof(struct TCB_t));
@@ -29,6 +48,13 @@ struct TCB_t* newItem()
 	return new_item;
 }
 
+
+/*
+* Function Name: AddQueue
+* Parameters: None
+* Return type: void
+* Description: Function to adds a queue item, pointed to by “item”, to the queue pointed to by head
+*/
 void AddQueue(struct TCB_t **head, struct TCB_t *item)
 {
 	struct TCB_t *temp = *head;
@@ -50,6 +76,12 @@ void AddQueue(struct TCB_t **head, struct TCB_t *item)
 	(*head)->pPrev = item;
 }
 
+/*
+* Function Name: DelQueue
+* Parameters: None
+* Return type: TCB_t*
+* Description: Function to delete an item from head and returns a pointer to the deleted item. If the queue is already empty, flag error
+*/
 struct TCB_t* DelQueue(struct TCB_t **head)
 {
 	struct TCB_t *back = *head;
@@ -81,6 +113,12 @@ struct TCB_t* DelQueue(struct TCB_t **head)
 	return deleted_element;
 }
 
+/*
+* Function Name: FreeItem
+* Parameters: None
+* Return type: void
+* Description: Function to free item
+*/
 void FreeItem(struct TCB_t *item)
 {
 	if(item != NULL)
