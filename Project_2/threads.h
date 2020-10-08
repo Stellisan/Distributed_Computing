b52e9ec15cb9ae8e889567a49fde16259c27dd29
Sethu Manickam <sethu.sxcs@gmail.com>
2020-10-07 18:58:02 -0700

    Update threads.h

--- Project_2/threads.h
+++ Project_2/threads.h
@@ -1,9 +1,21 @@
 #include "q.h"
 
+/*
+* Group members: 
+* 1. Sanjay Arivazhagan; ASURITE ID: 1217643921; sarivazh@asu.edu 
+* 2. Sethu Manickam; ASURITE ID: 1218452066; smanick4@asu.edu
+*/
+
 TCB_t* ReadyQ = NULL;
 TCB_t* Curr_Thread;
 int thread_counter = 0;
 
+/*
+* Function Name: start_thread
+* Parameters: *function
+* Return type: void
+* Description: Function to initialize queue of TCB_ts
+*/
 void start_thread(void (*function)(void))
 {
 	void *stack = malloc(8192);
@@ -15,8 +27,13 @@ void start_thread(void (*function)(void))
 	AddQueue(&ReadyQ, item);
 }
 
+/*
+* Function Name: run
+* Parameters: None
+* Return type: void
+* Description: Function to run the threads, get and switch context
+*/
 void run()
-
 {	// real code
 	Curr_Thread = DelQueue(&ReadyQ);
 	ucontext_t parent;     // get a place to store the main context, for faking
@@ -24,6 +41,12 @@ void run()
 	swapcontext(&parent, &(Curr_Thread->context));  // start the first thread
 }
 
+/*
+* Function Name: yield
+* Parameters: None
+* Return type: void
+* Description: Function to push and pop threads and switch context
+*/
 void yield()
 {
 	TCB_t* Prev_Thread;
