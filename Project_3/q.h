427e6f231af8503ccc81e1269c0751c23f55902d
Sethu Manickam <sethu.sxcs@gmail.com>
2020-10-26 00:22:09 -0700

    Update q.h

--- Project_3/q.h
+++ Project_3/q.h
@@ -38,7 +38,7 @@ void add_queue(TCB_t **head, TCB_t *item) {
 }
 
 /*
-* Function Name: add_queue
+* Function Name: delete_queue
 * Parameters: TCB_t **head
 * Return type: TCB_t *
 * Description: Function to delete an element from queue, and return deleted element
