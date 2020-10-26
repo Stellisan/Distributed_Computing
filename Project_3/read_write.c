51239ad3ba5354ea19e41d6fe404d1ed3f4169bb
SanArivu <sanjayarivazhagan3@gmail.com>
2020-10-25 23:33:40 -0700

    Corrections to semaphore structure

--- Project_3/read_write.c
+++ Project_3/read_write.c
@@ -7,75 +7,76 @@
 * 2. Sethu Manickam; ASURITE ID: 1218452066; smanick4@asu.edu
 * Used init_TCB() function as given by Dr. Dasgupta in Project 2 Instructions
 */
-
+#define GNU_SOURCE
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "sem.h"
 
-semaphore_t r_sem, w_sem, mutex;
+semaphore_t *r_sem, *w_sem, *mutex;
 int rwc = 0, wwc = 0, rc = 0, wc = 0, global_ID=0;
 
 void reader_entry(int ID)
 {
 	printf("[reader: #%d]\ttrying to read\n", ID);
-	P(&mutex);
+	P(mutex);
 	if (wwc > 0 || wc > 0) {
 	    printf("[reader: #%d]\tblocking for writer\n", ID);
 	    rwc++;		// increment waiting reader count.
-	    V(&mutex);		// let other processes use the mutex.
-	    P(&r_sem);		// sleep on r_sem
+	    V(mutex);		// let other processes use the mutex.
+	    P(r_sem);		// sleep on r_sem
 	    rwc--;		// program now has mutex and r_sem, go
 			        // into CS.
 	}
 	rc++;
 	if (rwc > 0)
-	    V(&r_sem);
+	    V(r_sem);
 	else
-	    V(&mutex);		// let other processes use the mutex.
+	    V(mutex);		// let other processes use the mutex.
 }
 
 void reader_exit(int ID)
 {
-	P(&mutex);
+	P(mutex);
 	rc--;			// i'm no longer a reader
 	if (rc == 0 && wwc > 0) {
 	    // if was the last reader, and there are waiting writers, open 
 	    // the w_sem door for them.
-	    V(&w_sem);
+	    V(w_sem);
 	} else
-	    V(&mutex);
+	    V(mutex);
 
 }
 
 void writer_entry(int ID)
 {
 	printf("[writer: #%d]\ttrying to write\n", ID);
-	P(&mutex);
+	P(mutex);
 	if (rc > 0 || wc > 0) {
 	    printf("[writer: #%d] blocking for others\n", ID);
 	    wwc++;		// increment waiting writers
-	    V(&mutex);		// let go of the mutex, since i'll be
+	    V(mutex);		// let go of the mutex, since i'll be
 	    // blocked
-	    P(&w_sem);		// wait in my line, when i wake up i DON'T 
+	    P(w_sem);		// wait in my line, when i wake up i DON'T 
 		 	    	// need a P(mutex since i've been given it 
 	    			// by the waking process.
 	    wwc--;		// i'm no longer waiting
 	}
 	wc++;			// increment writers
-	V(&mutex);		// let go of the mutex
+	V(mutex);		// let go of the mutex
 }
 
 void writer_exit(int ID)
 {
-	P(&mutex);
+	P(mutex);
 	wc--;
 	if (rwc > 0) {		// first, i let all the readers go.
-	    V(&r_sem);
+	    V(r_sem);
 	} else if (wwc > 0) {	// then i let another writer go
-	    V(&w_sem);
+	    V(w_sem);
 	} else {		// lastly, i give up the mutex
-	    V(&mutex);
+	    V(mutex);
 	}
 }
 
@@ -84,7 +85,7 @@ void writer_exit(int ID)
 void reader(void)
 { 
   int ID;
-  P(&mutex); ID = global_ID++; V(&mutex);
+  P(mutex); ID = global_ID++; V(mutex);
   while(1){
 	reader_entry(ID);
 	printf
@@ -97,7 +98,7 @@ void reader(void)
 void writer(void)
 {
   int ID;
-  P(&mutex); ID = global_ID++; V(&mutex);
+  P(mutex); ID = global_ID++; V(mutex);
   while(1){
  	writer_entry(ID);
 	printf
@@ -129,9 +130,9 @@ void writer(void)
 
 //Updated code
 int main() {
-    mutex= *CreateSem(1);
-    r_sem= *CreateSem(0);
-    w_sem= *CreateSem(0);
+    mutex= CreateSem(1);
+    r_sem= CreateSem(0);
+    w_sem= CreateSem(0);
     start_thread(reader);
     start_thread(reader);
     start_thread(reader);
