1e63cdd8c7af53429b129ed72cc045b031489012
Sethu Manickam <sethu.sxcs@gmail.com>
2020-10-26 00:34:56 -0700

    Update threads.h

--- Project_3/threads.h
+++ Project_3/threads.h
@@ -12,6 +12,11 @@ TCB_t *this_thread = NULL;
 const int STACK_SIZE = 8192;
 int ctr = 0;
 
+/*
+* Function Name: start_thread
+* Parameters: void(*function)(void)
+* Return type: void
+*/
 void start_thread (void(*function)(void)) {
 	void *stack =  malloc(STACK_SIZE);
 	TCB_t *tcb =  malloc(sizeof(TCB_t));
@@ -21,12 +26,22 @@ void start_thread (void(*function)(void)) {
 	add_queue (&ready_queue, tcb);
 }
 
+/*
+* Function Name: run
+* Parameters: No parameters
+* Return type: void
+*/
 void run() {
 	ucontext_t parent;
 	getcontext(&parent);
 	swapcontext(&parent, &(ready_queue -> context));
 }
 
+/*
+* Function Name: yield
+* Parameters: No parameters
+* Return type: void
+*/
 void yield() {
   	this_thread = delete_queue(&ready_queue);
   	add_queue(&ready_queue, this_thread);
